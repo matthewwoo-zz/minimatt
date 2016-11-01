98fa4f89108588fa6c30546a982b8eb9c08360ab
matthewwoo <matthew.edan.woo@gmail.com>
2016-11-01 08:50:45 -0700

    Testing oauth"

--- src/app.py
+++ src/app.py
@@ -7,6 +7,10 @@ from src import medium, dates
 
 app = Flask(__name__)
 
+@app.route('/')
+def start():
+    return "<p>test</p>"
+
 @app.route('/posts', methods=['GET'])
 def posts():
     posts = medium.get_posts(3)
@@ -17,7 +21,11 @@ def times():
     date_list = dates.get_dates(3)
     return flask.jsonify(date_list)
 
-
+@app.route('/auth', methods=['POST'])
+def auth():
+    if request.method == 'POST':
+        print request
+    return 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)

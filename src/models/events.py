05177bd2e0ca9aa2b7154630996430915ca6b58b
matthewwoo <matthew.edan.woo@gmail.com>
2016-11-14 08:59:52 -0800

    Adding date checker

--- src/models/events.py
+++ src/models/events.py
@@ -1,3 +1,5 @@
+import datetime
+
 
 class Event(object):
 
@@ -9,24 +11,78 @@ class Event(object):
         self.attendees,
         self.reminders
 
-    def get_freebusy():
-        the_datetime = datetime.datetime.utcnow()
-        the_datetime2 = the_datetime + datetime.timedelta(hours=1)
-        the_datetime1a = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S-08:00")
-        the_datetime2a = the_datetime2.strftime("%Y-%m-%dT%H:%M:%S-08:00")
-
-        body = {
-            "timeMin": the_datetime1a,
-            "timeMax": the_datetime2a,
-            "items": [{"id": "matthew.edan.woo@gmail.com"}, {"id": "matt@ujet.co"}]
-        }
-
-        eventsResult = service.freebusy().query(body=body).execute()
-        cal_dict = eventsResult[u'calendars']
-        for cal_name in cal_dict:
-            print(cal_name, cal_dict[cal_name])
+    # def get_freebusy(slot):
+    #     the_datetime = datetime.datetime.utcnow()
+    #     the_datetime2 = the_datetime + datetime.timedelta(hours=1)
+    #     the_datetime1a = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S-08:00")
+    #     the_datetime2a = the_datetime2.strftime("%Y-%m-%dT%H:%M:%S-08:00")
+    #
+    #     body = {
+    #         "timeMin": the_datetime1a,
+    #         "timeMax": the_datetime2a,
+    #         "items": [{"id": "matthew.edan.woo@gmail.com"}, {"id": "matt@ujet.co"}]
+    #     }
+    #
+    #     eventsResult = service.freebusy().query(body=body).execute()
+    #     cal_dict = eventsResult[u'calendars']
+    #     for cal_name in cal_dict:
+    #         print(cal_name, cal_dict[cal_name])
 
     def potential_slots(x):
+
+        # Generate time slots between 1:00 - 2:00pm and 5:00 - 5:30pm on weekdays
+        today = datetime.datetime.now()
+        while today.weekday() > 4 and today.weekday() < 6:
+            today = datetime.timedelta(days=1)
+        start_day = today + datetime.timedelta(days=1)
+
+        period_1_start_hour = 1
+        period_1_end_minutes = 30
+
+        period_2_start_hour = 5
+        period_2_end_minutes = 30
+
+        i = 0
+        slot_list = []
+        while i < x:
+            slot_1_start = datetime.datetime(
+                start_day.year,
+                start_day.month,
+                start_day.day,
+                period_1_start_hour,
+            )
+
+            slot_1_end = datetime.datetime(
+                start_day.year,
+                start_day.month,
+                start_day.day,
+                period_1_end_minutes
+            )
+
+            if get_freebusy(slot_1_start, slot_1_end) == True:
+                ...
+
+            slot1 = slot.strftime("%Y-%m-%dT%H:%M:%S-08:00")
+
+
+            slot_list.append(slot)
+
+
+
+            i += 1
+
+
+
+
+
+            = datetime.datetime(start_day.year, start_day.month, start_day.day, 8)
+        time_period_2 = datetime.datetime
+
+
+
+            start = today + datetime.timedelta(days=1)
+            start = start.
+
         the_datetime = datetime.datetime.utcnow()
         the_datetime2 = the_datetime + datetime.timedelta(hours=1)
         the_datetime1a = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S-08:00")

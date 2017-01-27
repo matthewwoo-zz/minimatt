212fd45092c319c7ae94fe44006dfd877720383d
matthewwoo <matthew.edan.woo@gmail.com>
2017-01-27 10:22:53 -0800

    Updated to succesfully post free times back to Chatfuel

--- src/app.py
+++ src/app.py
@@ -15,37 +15,14 @@ CLIENT_SECRET_FILE = '/Users/mattw/Dropbox/Programming/MiniWooBot/client_secret.
 
 @app.route('/')
 def home():
+    print "request going through"
     return "<p>Running</p>"
 
-@app.route('/slots')
-def slots():
-    if 'credentials' not in flask.session:
-        return flask.redirect(flask.url_for('oauth2callback'))
-    credentials = client.OAuth2Credentials.from_json(flask.session['credentials'])
-    if credentials.access_token_expired:
-        return flask.redirect(flask.url_for('oauth2callback'))
-    else:
-        http_auth = credentials.authorize(httplib2.Http())
-        service = discovery.build('calendar', 'v3', http_auth)
-        cal = Calendar()
-        today = cal.date_range()
-        busy_slots = cal.busy_slots(service=service, body=today)
-        slots = cal.potential_slot()
-        free_slots = []
-        i = 0
-        while i <= 1:
-            if cal.check_slot(slots[i], busy_slots):
-                i += 1
-            free_slots.append(slots[i])
-            i += 1
-        json_slots = cal.post_dates(free_slots)
-        return flask.jsonify(json_slots)
-
-@app.route('/oauth2callback')
-def oauth2callback():
+@app.route('/login')
+def login():
     flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE,
-        scope=SCOPES,
-        redirect_uri=flask.url_for('oauth2callback', _external=True))
+                                          scope=SCOPES,
+                                          redirect_uri=flask.url_for('login', _external=True))
     if 'code' not in flask.request.args:
         auth_uri = flow.step1_get_authorize_url()
         return flask.redirect(auth_uri)
@@ -53,7 +30,44 @@ def oauth2callback():
         auth_code = flask.request.args.get('code')
         credentials = flow.step2_exchange(auth_code)
         flask.session['credentials'] = credentials.to_json()
-        return flask.redirect(flask.url_for('slots'))
+        print credentials.to_json()
+        return "200"
+
+@app.route('/slots')
+def slots():
+    credentials = client.AccessTokenCredentials('ya29.GlvgAyARY23-wOXykO9QmM7cZl3ZvTW7flyMUmZM3gIhhtnc8kbCWlWmuMqYyxAytyuorGNN3ZXM6w-6d01UEl-gt5G3iEpfNr7T8paVp41u8_WPtM3p1W3gG8Nm',
+                                                'my-user-agent/1.0')
+    print "credentials value assigned"
+    print credentials
+    http_auth = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http_auth)
+    cal = Calendar()
+    today = cal.date_range()
+    busy_slots = cal.busy_slots(service=service, body=today)
+    slots = cal.potential_slot()
+    free_slots = []
+    i = 0
+    while i <= 1:
+        if cal.check_slot(slots[i], busy_slots):
+            i += 1
+        free_slots.append(slots[i])
+        i += 1
+    json_slots = cal.post_dates(free_slots)
+    return flask.jsonify(json_slots)
+
+# @app.route('/oauth2callback')
+# def oauth2callback():
+#     flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE,
+#         scope=SCOPES,
+#         redirect_uri=flask.url_for('oauth2callback', _external=True))
+#     if 'code' not in flask.request.args:
+#         auth_uri = flow.step1_get_authorize_url()
+#         return flask.redirect(auth_uri)
+#     else:
+#         auth_code = flask.request.args.get('code')
+#         credentials = flow.step2_exchange(auth_code)
+#         flask.session['credentials'] = credentials.to_json()
+#         return flask.redirect(flask.url_for('slots'))
 
 
 @app.route('/posts', methods=['GET'])
